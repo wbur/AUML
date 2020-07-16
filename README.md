34758fcc0959d74547ab5a23a4d88808b7f991dc
CrookedNumber <david@crookednumber.com>
2020-07-16 14:09:54 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,74 @@
+# AUML
+
+AUML is a simple regex-like syntax for turning a few sentences into 100s or 1000s of Amazon Alexa skill utterances via a Python parser. It also allows you to keep, store and maintain all your intents and utterances outside of the Alexa GUI. Finally, it allows you to work around the fact that an utterance can accept only a single slot.
+
+## Installation
+
+1. Clone/download the repo.
+2. Copy data.py.example to a new file called data.py: `cp data.py.example data.py`
+3. Make sure that parse.py is executable: `chmod u+x parse.py`
+4. Adjust the `intents` and (if needed) `variables` dictionaries within data.py to suit your skill.
+5. Run the parsing script to generate utterances: `python parse.py`
+6. (If you're on OSX, the all the utterances will be automatically inserted into your clipboard.)
+7. Insert utterances into your skill (via Devloper Console bulk edit tool, ASK-SDK, etc.)
+
+## Usage
+
+Each intent element can be a string or a list of strings, representing a single intent. Each string represents a base utterance that then can be parsed into multiple utterances.
+
+Each element can be a string or a list of strings, representing a single intent. Each string represents an base utterance that can be parsed into multiple utterances.
+
+The syntax (AUML) can include the following:
+
+* simple, literal words
+* optional words, followed by a ?
+* a list of OR'ed words, offset by () and separated by a |
+* Alexa literal slots, offset of course by {}
+* variables, begins with $ (see below)
+
+For example, a ‘stream’ intent, which allows a listener to access an FM livestream, could look like this within data.py:
+
+```
+intents = {
+	'stream': [
+		"{stream}"
+		"listen live?",
+		"$play the? {stream}"
+	]
+}
+```
+
+In that last string, `$play` maps to a variable, like
+
+```
+variables = {
+	'play': "(play|play me|I want to listen to)"
+}
+```
+
+which means that string
+
+`$play the? {stream}`
+
+will first become
+
+`(play|play me|I want to listen to) the? {stream}`
+
+This, in turn, will yield the following utterances when parsed.
+
+```
+play {stream}
+play me {stream}
+I want to listen to {stream}
+play the {stream}
+play me the {stream}
+I want to listen to the {stream}
+```
+
+Variables, in other words, are a way to reuse phrases across multiple intents and base utterances. It also allows one to sidestep the Alexa rule of only one slot per utterance.
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+MIT
---
+++ parse.py
@@ -0,0 +1,81 @@
+import re, os, platform
+from data import Data
+
+data = Data()
+
+intents = data.intents
+variables = data.variables
+
+intent_list = list(intents.keys())
+limit = len(intents)
+prompt = "Pick an intent [1-" + str(limit) + "]\n"
+for i in range(limit):
+    prompt = prompt + str(i+1) + ") " + intent_list[i] + "\n"
+
+choice = int(input(prompt)) - 1
+
+sentences = intents[intent_list[choice]]
+
+if type(sentences) == str:
+    sentences = [sentences]
+
+all_utterances = []
+for sentence in sentences:
+
+    for key in variables:
+        sentence = sentence.replace('$' + key, variables[key])
+
+    #pieces = AUML.split(' ')
+    # see https://stackoverflow.com/questions/9644784/splitting-on-spaces-except-between-certain-characters
+    pieces = re.split(r"\s+(?=[^()]*(?:\(|$))", sentence)
+    utterances = [[]]
+
+    for piece in pieces:
+        if piece[0] == '(' and piece[-1] == ')':
+            fragments = piece[1:-1].split('|')
+            new_utterances = []
+            for index, utterance in enumerate(utterances):
+                original_utterance = utterances[index]
+                for fragment in fragments:
+                    #use copy of original_utterance or it all goes to hell
+                    new_utterance = list(original_utterance)
+                    new_utterance.append(fragment)
+                    new_utterances.append(new_utterance)
+            utterances = new_utterances
+        elif piece[-1] == '?':
+            word = piece[0:-1]
+            temp = []
+            for index, utterance in enumerate(utterances):
+                # keep the pattern WITHOUT the optional word
+                temp.append(list(utterances[index]))
+                # but also add the optional word to another branch
+                utterances[index].append(word)   
+            utterances.extend(temp)
+        else:
+            for index, utterance in enumerate(utterances):
+                utterances[index].append(piece)
+    all_utterances.extend(utterances)
+
+all_utterances = [' '.join(x) for x in all_utterances]
+
+list_set = set(all_utterances) 
+# convert the set to the list 
+unique_list = (list(list_set))
+
+print ('***********************************')
+print ('***********************************')
+print ('Creating ' + str(len(all_utterances)) + ' utterances')
+print ('***********************************')
+print ('***********************************')
+
+clip = ''
+for utterance in unique_list:
+    print (utterance)
+    clip = clip + utterance + "\r"
+
+if platform.system() == 'Darwin':
+    # if you’re on a mac
+    # all the utterances will be inserted
+    # into your clipboard
+    cmd = 'echo "%s" | pbcopy' % clip
+    os.system(cmd)
